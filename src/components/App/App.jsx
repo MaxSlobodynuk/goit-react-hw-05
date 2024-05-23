4b47eeb1f657438b9ffcf8c31c9c718294015bd4
Max Slobodyanuk <119804658+MaxSlobodynuk@users.noreply.github.com>
2024-05-23 16:48:12 +0300

    add routing between pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../../pages/HomePage/HomePage";
+import MoviesPage from "../../pages/MoviesPage/MoviePage";
+import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
+import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage";
+import Navigation from "../Navigation/Navigation";
+
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Navigation />
 
-export default App
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
